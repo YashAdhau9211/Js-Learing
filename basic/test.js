813b67bb851ec82ba22734d7042f6846217b2d69
YashAdhau9211 <143120526+YashAdhau9211@users.noreply.github.com>
2023-12-17 13:05:32 +0000

    TESTING JS BASIC

---
+++ basic/test.js
@@ -0,0 +1,21 @@
+// console.log("Hello World!");
+
+
+const accountNumber = 1000;
+let accountName = "YASH ADHAU";
+var accounttype = "Savings";
+
+let accountStates ;
+
+// accountNumber = 2000;
+accountName="Purva Adhau"
+accounttype="current"
+
+let anyNumber = 100
+
+console.log(--anyNumber);
+
+console.log((accountNumber));
+console.log((accountStates));
+console.log((accountName));
+console.log((accounttype));
