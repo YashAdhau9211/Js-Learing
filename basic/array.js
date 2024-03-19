b1a460eb9bee19256dbafbf68d4654eaeafaa883
YashAdhau9211 <143120526+YashAdhau9211@users.noreply.github.com>
2024-03-19 20:22:56 +0000

    array part 2

--- basic/array.js
+++ basic/array.js
@@ -1,15 +1,26 @@
 const myArr = [0,1,2,3,4,5,6]
-const myName = ["S","N","E","H","A"]
+const myHer = ["S","N","E","H","A"]
+const myMe = ["Y","A","S","H"]
 
 //console.log(myArr[0]);
 
-const newArr = myName.join()
+//onst newArr = myName.join()
 
 
 //myArr.push(7)
-myArr.unshift(7)
-console.log(myArr);
-console.log(myArr[0]);
-console.log(myName);
-console.log(myName.includes("S"));
-console.log(typeof(newArr));
+// myArr.unshift(7)
+// console.log(myArr);
+// console.log(myArr[0]);
+// console.log(myName);
+// console.log(myName.includes("S"));
+// console.log(typeof(newArr));
+
+
+// myHere.push(myMe)
+// console.log(myHere);
+
+const myWorld = [...myHer,...myMe]
+console.log(myWorld);
+
+console.log(Array.isArray("YASH"));
+console.log(Array.from("YASH"));
