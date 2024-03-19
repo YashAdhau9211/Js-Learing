5f0b9c4c4dd54b3402948bdeb8de5b19d7b837bc
YashAdhau9211 <143120526+YashAdhau9211@users.noreply.github.com>
2024-03-19 20:05:10 +0000

    array

---
+++ basic/array.js
@@ -0,0 +1,15 @@
+const myArr = [0,1,2,3,4,5,6]
+const myName = ["S","N","E","H","A"]
+
+//console.log(myArr[0]);
+
+const newArr = myName.join()
+
+
+//myArr.push(7)
+myArr.unshift(7)
+console.log(myArr);
+console.log(myArr[0]);
+console.log(myName);
+console.log(myName.includes("S"));
+console.log(typeof(newArr));
